8929016bdf225a30a7ac9b28cf38728328bdef82
shihaipeng_glb <shihaipeng_glb@kgc.com>
2020-11-25 16:03:53 +0800

    TestFile

--- src/main/java/com/kgc/gitdemo/test/Test.java
+++ src/main/java/com/kgc/gitdemo/test/Test.java
@@ -1,7 +1,1 @@
-package com.kgc.gitdemo.test;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Git");
-    }
-}
+package com.kgc.gitdemo.test;public class Test {    public static void main(String[] args) {        System.out.println("Git");    }}
